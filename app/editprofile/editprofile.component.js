1d6f7d57b2d9170e911f377c5876816002f1cfc4
Kathy <kpnelson847@gmail.com>
2019-02-15 00:30:55 -0500

    finished the css for the edit profile page

--- app/editprofile/editprofile.component.js
+++ app/editprofile/editprofile.component.js
@@ -2,14 +2,15 @@
 const editprofile = {
     template: `
     <form ng-submit="$ctrl.newProfile($ctrl.newProf)">
-        <p>Name</p>
-        <input type="text" placeholder="Name" ng-model="$ctrl.newProf.name">
-        <p>Contact</p>
-        <input type="text" placeholder="Contact" ng-model="$ctrl.newProf.contact">
-        <p>Bio</p>
-        <input type="textarea" placeholder="Bio"  ng-model="$ctrl.newProf.bio">
+        <p class="description" >Use the form below to update your profile</p>
+        <p class="nameP">Name</p>
+        <input class="nameInput" type="text" placeholder="Name" ng-model="$ctrl.newProf.name">
+        <p class="contactP" >Contact</p>
+        <input class="contactInput" type="text" placeholder="Contact" ng-model="$ctrl.newProf.contact">
+        <p class="bioP">Bio</p>
+        <input class="bioInput" type="textarea" placeholder="Bio"  ng-model="$ctrl.newProf.bio">
         
-        <button>Update</button>
+        <button class="updateButton">Update</button>
     </form>
     `,
     controller: ["ProfileService", function(ProfileService) {
